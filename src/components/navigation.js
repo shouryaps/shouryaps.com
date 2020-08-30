419890fc9c9b475062b57a5f8e920e1d458338dc
Shourya Pratap Singh <shourya4@gmail.com>
2020-08-30 22:25:36 +0530

    Removed contact and not required pages

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -15,10 +15,6 @@ const MenuItems = [
     path: "/blog",
     title: "Blog"
   },
-  {
-    path: "/contact",
-    title: "Contact"
-  },
 ]
 
 const ListLink = (props) => (<li><Link to={props.to}>{props.children}</Link></li>)
