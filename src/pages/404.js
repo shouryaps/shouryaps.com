52750727c0f5d59ab01077a53227635eb839ad68
Shourya Pratap Singh <shourya4@gmail.com>
2020-08-30 22:23:59 +0530

    Updated 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import {RiArrowLeftSLine, RiBugLine, RiSkullLine} from "react-icons/ri"
+import {RiArrowLeftSLine, RiSkullLine} from "react-icons/ri"
 
 import SEO from "../components/seo"
 import Layout from "../components/layout"
@@ -16,11 +16,10 @@ const NotFound = () => (
           fontSize: "128px",
           color: "var(--primary-color)"
         }}/>
-        <h1>Oops we did not expect that to happen</h1>
-        <p>Have you wondered into the unknow. Let us help you, Please take a look at below options</p>
+        <h1>Oops I did not expect that to happen</h1>
+        <p>Have you wondered into the unknown?</p>
       </header>
       <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Back to Homepage</Link>
-      <Link to="/contact" className="button -outline">Report this <RiBugLine className="icon -right"/></Link>
     </div>
   </Layout>
 )
